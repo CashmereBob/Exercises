9fd3a8ff37b05d8726183ea2ea9c141fea439959
CashmereBob <weidmar@gmail.com>
2016-03-01 08:41:37 +0100

    Extra uppgift modul 4

---
+++ ExtraMod4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraMod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = 10;
+            var htmlOutput = new StringBuilder();
+
+            htmlOutput.Append("<table>\n");
+
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    htmlOutput.Append(" <tr>\n");
+                }
+                else {
+                    htmlOutput.Append(" <tr bgcolor=\"#CCCCCC\">\n");
+                }
+
+                htmlOutput.Append("     <td>Row" + i + "</td>\n");
+                htmlOutput.Append("     <td>xxxxx</td>\n");
+                htmlOutput.Append(" </tr>\n");
+            }
+
+            htmlOutput.Append("</table>\n");
+
+            string html = htmlOutput.ToString();
+
+            Console.WriteLine(html);
+            Console.ReadLine();
+        }
+    }
+}
