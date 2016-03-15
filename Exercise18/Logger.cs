ec063fc46dea3998b44e1e69d597de8ef88ec9d3
CashmereBob <weidmar@gmail.com>
2016-03-15 16:50:35 +0100

    renames

---
+++ Exercise18/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise18
+{
+    class Logger
+    {
+        private static List<string> logPosts = new List<string>();
+        public static void Log(string message)
+        {
+            // Add the message to a private List<string>
+            logPosts.Add(message);
+        }
+
+        public static void PrintLog()
+        {
+            Console.Clear();
+            Console.WriteLine("Logg: ");
+            Console.WriteLine();
+            foreach (string post in logPosts)
+            { Console.WriteLine(post); }
+            Console.WriteLine();
+            Console.Write("Press any key to go back.");
+            Console.ReadKey();
+
+        }
+
+    }
+}
